2edb67ce9b683943c03b43fb338ec06d73ae0595
Rodolpho Alves <Lyulie@github.com>
2021-06-21 18:36:24 -0300

    manipulando estados

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,9 +1,13 @@
-type ButtonProps = {
-    children ?: string
-}
+import { useState } from "react"
 
-export function Button(props : ButtonProps) {
+export function Button() {
+    const [counter, setCounter] = useState(0)
+
+    function increment() {
+        setCounter(counter + 1)
+    }
+
     return(
-        <button>{props.children || "nada"}</button>
+        <button onClick={increment}>{counter}</button>
     )
 }
